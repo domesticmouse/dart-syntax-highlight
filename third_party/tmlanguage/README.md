466c71af97b424c512ec93ce2ee5252b4c87c08f
Brett Morgan <brettmorgan@google.com>
2023-03-08 16:42:38 +1000

    Verify grammar with JSON Schema (broken)

---
+++ third_party/tmlanguage/README.md
@@ -0,0 +1,5 @@
+# `tmLanguage` Schema
+
+This is a JSON schema for TextMate grammar definitions.
+
+Can be used to get intellisense working when editing grammar definitions within **Visual&nbsp;Studio&nbsp;Code**.
--- tool/verify.dart
+++ tool/verify.dart
@@ -5,12 +5,20 @@
 import 'dart:convert';
 import 'dart:io';
 
-void main() {
+import 'package:json_schema2/json_schema2.dart';
+
+void main() async {
+  final schema = await JsonSchema.createSchemaAsync(
+      await File('third_party/tmlanguage/tmlanguage.json').readAsString(),
+      schemaVersion: SchemaVersion.draft4);
+
   // Validate the grammars/dart.json file.
   File grammarFile = File('grammars/dart.json');
 
   try {
-    jsonDecode(grammarFile.readAsStringSync());
+    final grammar = grammarFile.readAsStringSync();
+    jsonDecode(grammar);
+    schema.validate(grammar);
     print('${grammarFile.path} ok.');
     exit(0);
   } catch (e) {
